b2fa54a211d2cd91fb63c07ea97bc59dcee7f204
Ivan DING <ivan.ding@webpowerchina.com>
2018-12-03 11:45:43 +0800

    ext init

---
+++ modules/ExtModule.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: robbie
+ * Date: 2018/12/3
+ * Time: 11:43
+ */
+
+namespace app\modules\ext;
+
+class ExtModule extends \yii\base\Module{
+
+    public $controllerNamespace = 'app\modules\ext\controllers';
+
+    public function init()
+    {
+        parent::init();
+
+
+        // custom initialization code goes here
+    }
+}
